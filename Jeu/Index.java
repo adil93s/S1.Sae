a75e8a9536652da039a5f6fe0abd7dfde26a5896
Adil CHETOUANI <adil@MacBook-Pro-de-Adil.local>
2021-12-03 20:46:36 +0100

    Premiere version

--- Jeu/Index.java
+++ Jeu/Index.java
@@ -1,8 +1,18 @@
+package Jeu;
+
 import java.util.Scanner;
 
 public class Index {
     public static void main(String[] args) {
+        char[][] zone = new char[6][7];
 
+        for (int ligne = 0; ligne < zone.length; ligne++) {
+            for (int colonne = 0; colonne < zone[ligne].length; colonne++) {
+                zone[ligne][colonne] = '.';
+            }
+        } 
+
+        System.out.println(zone);
     }
 
 }
