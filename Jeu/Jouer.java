fad0a9c045b30d5b9ad274fee3ef4875183dc4d8
igada <ismailgada94@gmail.com>
2021-12-16 12:09:44 +0100

    change ligne 8

--- Jeu/Jouer.java
+++ Jeu/Jouer.java
@@ -1,15 +1,38 @@
-import java.io.InputStream;
-import java.util.Scanner;
 package Jeu;
+import java.util.Scanner;
 
 public class Jouer {
-    public static void Play (String [][]){
+    public static void main (String[] args){
+        Play();
+    }
+    public static void Play () {
         Scanner sc = new Scanner (System.in);
         String x;
+        String[] Lettre = {"A","B","C","D"};
         int y;
+        boolean acces = true;
+        while (acces){
+            x = sc.nextLine();
+            for( int i = 0; i < Lettre.length;i++){
+                if(Lettre[i] == x){
+                    acces = false;
+                }
+                else{
+                    System.out.println("Erreur!! Vous avez rentrer une valeur non valide faîtes attention!  X");
+                    acces = false;
+                }
+            }
+        }
+
+
+
+
         System.out.println("Veuillez entrer une position (ligne puis colonne) de l'endroit ou vous pensez que le bateau ennemie se situe! Bonne chance Mousaillons!!");
-        x= (sc.nextLine());
+
         y= Integer.parseInt(sc.nextLine());
-        while (y>10 || y<0 || x!=) ;
+        while (y<0 || y>10){
+            System.out.println("Erreur!! Vous avez rentrer une valeur non valide faîtes attention!  Y");
+            y= Integer.parseInt(sc.nextLine());
+        }
     }
 }
