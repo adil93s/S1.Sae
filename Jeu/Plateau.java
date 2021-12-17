ced5f66947fb8f0f8cc946dc5e92d137770a4588
onizuka93 <42942677+onizuka93@users.noreply.github.com>
2021-12-17 11:47:25 +0100

    Update Plateau.java

--- Jeu/Plateau.java
+++ Jeu/Plateau.java
@@ -2,44 +2,86 @@ package Jeu;
 
 public class Plateau{
     public static void main(String[] args) {
-        situation = initialisation();
-        afficherP(situation);
+        launch();
     }
-    static int[][] situation = new int[11][11];
+
+    public static void launch() {
+        joueur1 = initialisation(joueur1);
+        joueur2 = initialisation(joueur2);
+        System.out.println("\nPlateau du J1\n");
+        afficherGrille(joueur1);
+        System.out.println("\nPlateau du J2\n");
+        afficherGrille(joueur2);
+    }
+
+    static String[][] joueur1 = new String[11][11];
+    static String[][] joueur2 = new String[11][11];
     static boolean fini_jeu;
-    public static int[][] initialisation() {
-        int i, j, x,  y;
-        for (i = 0; i < 10; i = i + 1) {
-            for (j = 0; j < 10; j = j + 1) {
-                situation[i][j] = 0;
+
+    public static String[][] initialisation(String[][] situation) {
+        int ligne;
+        int colonne;
+        for (ligne = 0; ligne < situation.length - 1; ligne++) {
+            for (colonne = 0; colonne < (situation[ligne].length - 1); colonne++) {
+                situation[ligne][colonne] = "~";
             }
         }
-        x = (int) (Math.random() * 10);
-        y = (int) (Math.random() * 10);
-        situation[x][y] = 1 - situation[x][y];
-        situation[x][y - 1] = 1 - situation[x][y - 1];
+        int x = 0; 
+        int y = 0; 
+        for (int compteur = 0; compteur < 10; compteur++) {
+            x = (int) (Math.random() * 10);
+            y = (int) (Math.random() * 10);
+            if(y != 0) {
+                situation[x][y] = "□";
+                situation[x][y - 1] = "□";
+            }
+        }
         fini_jeu = false;
         return situation;
     }
-    // Déclaration de la grille
-    public static void afficherP(int[][] situation) {
-        int i;
-        int j;
-        System.out.println("   A B C D E F G H I J");
-        for (i = 0; i < 10; i = i + 1) {
-            System.out.print(i + "  ");
-            for (j = 1; j < 11; j = j + 1) {
-                if (situation[i][j] == 0) {
-                    System.out.print("~ ");
+
+    public static void afficherGrille(String[][] situation) {
+        int ligne;
+        int colonne;
+        System.out.println("     A B C D E F G H I J");
+        for (ligne = 0; ligne < (situation.length - 1); ligne++) {
+            if (ligne == 0) {
+                for (int repeat = 0; repeat < 26; repeat++) {
+                    if (repeat < 3)
+                        System.out.print(" ");
+                    else
+                        System.out.print("■");
                 }
-                if (situation[i][j] == 2) {
-                    System.out.print("* ");
+                System.out.println();
+            }  
+            if (ligne != 9) {
+                System.out.print((ligne+1) + "  ");
+            } else {
+                System.out.print((ligne+1) + " ");
+            }
+            for (colonne = 0; colonne < (situation[ligne].length - 1); colonne++) {
+                if (colonne == 0) {
+                    System.out.print("■ ");
+                    System.out.print(situation[ligne][colonne] + " ");
+                } else if (colonne == 9) {
+                    System.out.print(situation[ligne][colonne] + " ");
+                    System.out.print("■"); 
                 }
-                if (situation[i][j] == 1) {
-                    System.out.print("X ");
+                else {
+                    System.out.print(situation[ligne][colonne] + " ");
                 }
             }
             System.out.println();
         }
+        for (int repeat = 0; repeat < 26; repeat++) {
+            if (repeat < 3)
+                System.out.print(" ");
+            else
+                System.out.print("■");
+        }
+        System.out.println();      
     }
+
+
+
 }
