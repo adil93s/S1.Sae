186a9fa0daf50796db22768dfc609cb86af28a69
igada <ismailgada94@gmail.com>
2021-12-17 11:36:07 +0100

    VF

--- Jeu/Jouer.java
+++ Jeu/Jouer.java
@@ -2,37 +2,24 @@ package Jeu;
 import java.util.Scanner;
 
 public class Jouer {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Play();
     }
-    public static void Play () {
-        Scanner sc = new Scanner (System.in);
-        String x;
-        String[] Lettre = {"A","B","C","D"};
-        int y;
-        boolean acces = true;
-        while (acces){
-            x = sc.nextLine();
-            for( int i = 0; i < Lettre.length;i++){
-                if(Lettre[i] == x){
-                    acces = false;
-                }
-                else{
-                    System.out.println("Erreur!! Vous avez rentrer une valeur non valide faîtes attention!  X");
-                    acces = false;
-                }
+        public static void Play () {
+            Scanner sc = new Scanner(System.in);
+            char x;
+            System.out.println("Veuillez entrer une position ligne  de l'endroit ou vous pensez que le bateau ennemie se situe! Bonne chance Mousaillons!! (Lettre en Majuscule)");
+            boolean acces = true;
+            x = sc.nextLine().charAt(0);
+            while (x > 'j') {
+                System.out.println("Erreur!! Vous avez rentrer une valeur non valide faîtes attention!  X");
+                x = sc.nextLine().charAt(0);
+            }
+            System.out.println("Veuillez entrer une position colonne de l'endroit ou vous pensez que le bateau ennemie se situe! Bonne chance Mousaillons!! (entre 1 et 10)");
+            int y = Integer.parseInt(sc.nextLine());
+            while (y < 0 || y > 10) {
+                System.out.println("Erreur!! Vous avez rentrer une valeur non valide faîtes attention!  Y");
+                y = Integer.parseInt(sc.nextLine());
             }
         }
-
-
-
-
-        System.out.println("Veuillez entrer une position (ligne puis colonne) de l'endroit ou vous pensez que le bateau ennemie se situe! Bonne chance Mousaillons!!");
-
-        y= Integer.parseInt(sc.nextLine());
-        while (y<0 || y>10){
-            System.out.println("Erreur!! Vous avez rentrer une valeur non valide faîtes attention!  Y");
-            y= Integer.parseInt(sc.nextLine());
-        }
-    }
 }
