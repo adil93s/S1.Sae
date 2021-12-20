939358f7ce46e6f1960dbbc6d19f3e15eebb9cd0
IsmailSKR <95474812+IsmailSKR@users.noreply.github.com>
2021-12-20 15:42:14 +0100

    Update Jouer.java

--- Jeu/Jouer.java
+++ Jeu/Jouer.java
@@ -1,4 +1,4 @@
-package Jeu;
+//package Jeu;
 import java.util.Scanner;
 
 public class Jouer {
@@ -11,13 +11,13 @@ public class Jouer {
             System.out.println("Veuillez entrer une position ligne  de l'endroit ou vous pensez que le bateau ennemie se situe! Bonne chance Mousaillons!! (Lettre en Majuscule)");
             boolean acces = true;
             x = sc.nextLine().charAt(0);
-            while (x > 'j') {
+            while (x > 'j' || x > 'J') {
                 System.out.println("Erreur!! Vous avez rentrer une valeur non valide faîtes attention!  X");
                 x = sc.nextLine().charAt(0);
             }
             System.out.println("Veuillez entrer une position colonne de l'endroit ou vous pensez que le bateau ennemie se situe! Bonne chance Mousaillons!! (entre 1 et 10)");
             int y = Integer.parseInt(sc.nextLine());
-            while (y < 0 || y > 10) {
+            while (y < 1 || y > 10) {
                 System.out.println("Erreur!! Vous avez rentrer une valeur non valide faîtes attention!  Y");
                 y = Integer.parseInt(sc.nextLine());
             }
