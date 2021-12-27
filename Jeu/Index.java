dcbc92d5d1a43b044d2c3ca03a70e6af6fea7e12
onizuka93 <42942677+onizuka93@users.noreply.github.com>
2021-12-28 00:12:35 +0100

    Update Index.java

--- Jeu/Index.java
+++ Jeu/Index.java
@@ -1,106 +1,99 @@
+import java.io.*;
 import java.util.Scanner;
-public class Index { 
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-      
-    static String[][] j1Grille = Grille.creerGrille();
-    static String[][] j2Grille = Grille.creerGrille();
-    static boolean naviretouche = true;
-    static boolean fini_jeu = false;
+public class Menu {
 
-    public static boolean verifFinJeu(String[][] grille) {
-        for (int ligne = 0 ; ligne < grille.length ; ligne++) {
-            for (int colonne = 0 ; colonne < grille[ligne].length ; colonne++) {
-                if (grille[ligne][colonne] == "□") {
-                    return false;
-                }
-            }
+    public final static void clearConsole() {
+        try {
+            if(System.getProperty("os.name" ).startsWith("Windows" ))
+              Runtime.getRuntime().exec("cls" );
+            else
+              Runtime.getRuntime().exec("clear" );
+        } catch (Exception excpt) {
+            for(int i=0; i < 100; i++)
+              System.out.println();
         }
-        return true;
-    }    
+    }
 
-    public static void Tirer(String[][] grille, String j1, String j2) {
+    public static void afficherEnLigne(char c, int longueur) {
+	    for (int colonne = 1; colonne <= longueur; colonne++) {
+			System.out.print(c);
+		}
+	}
+
+	public static void afficherEnLigneln(char c, int longueur) {
+		afficherEnLigne(c, longueur);
+		System.out.println();
+	}
+
+
+    public static String message() {
+        try {
+            File file = new File("Message.txt");    
+            FileReader fr = new FileReader(file);  
+            BufferedReader br = new BufferedReader(fr);  
+            StringBuffer sb = new StringBuffer();    
+            String line;
+            while((line = br.readLine()) != null)
+            {
+                sb.append(line);      
+                sb.append("\n");     
+            }
+            fr.close();    
+            return sb.toString();  
+        } catch(IOException e) {
+            e.printStackTrace();
+            return "error";
+        }
+    }
+
+    public static void messageMenu() {
+        int hauteur = 10;
+        int largeur = 40;
+        afficherEnLigne(' ', 30);
+        afficherEnLigneln('■', largeur);
+        for (int ligne = 2; ligne < hauteur; ligne++) {
+            afficherEnLigne(' ', 30);
+            afficherEnLigne('■', 1);
+            if (ligne == 5) {
+                afficherEnLigne(' ', 9);
+                System.out.print("1. Lancer une partie");
+                afficherEnLigne(' ', 9);
+            } else if (ligne == 6) {
+                afficherEnLigne(' ', 14);
+                System.out.print("2. Quitter");
+                afficherEnLigne(' ', 14);
+            } else {
+                afficherEnLigne(' ', largeur - 2);
+            }
+            afficherEnLigneln('■', 1);
+        }
+        afficherEnLigne(' ', 30);
+        afficherEnLigneln('■', largeur);
+	}    
+
+    public static void launch() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Veuillez entrer la position colonne de l'endroit ou vous pensez que le bateau ennemie se situe! Bonne chance Mousaillons!! (entre 1 et 10)");
-        int x = Integer.parseInt(sc.nextLine());
-        while (x < 1 || x > 10) {
-            System.out.println("Erreur!! Vous avez rentrer une valeur non valide faîtes attention!  X");
-            x = Integer.parseInt(sc.nextLine());
-        }
-        System.out.println("Veuillez entrer une position ligne de l'endroit ou vous pensez que le bateau ennemie se situe! Bonne chance Mousaillons!! (entre 1 et 10)");
-        int y = Integer.parseInt(sc.nextLine());
-        while (y < 1 || y > 10) {
-            System.out.println("Erreur!! Vous avez rentrer une valeur non valide faîtes attention!  Y");
-            y = Integer.parseInt(sc.nextLine());
-        }
-
-        if (grille[y-1][x-1] == "□") {
-            grille[y-1][x-1] = ANSI_RED + "X";
-            System.out.println("\nNavire ennemi touché !\n");
-            naviretouche = true;
-            fini_jeu = verifFinJeu(grille);
-            System.out.println("\nGrille de " + j1 + " :\n");
-            Grille.afficherGrille(j1Grille);
-            System.out.println("\nGrille de " + j2 + " :\n");
-            Grille.afficherGrille(j2Grille);
-        } else {
-            grille[y-1][x-1] = ANSI_CYAN + "X";
-            System.out.println("\nAucun navire touché !\n");
-            naviretouche = false;
-            System.out.println("\nGrille de " + j1 + " :\n");
-            Grille.afficherGrille(j1Grille);
-            System.out.println("\nGrille de " + j2 + " :\n");
-            Grille.afficherGrille(j2Grille);
-        }
-    }
-
-    public static void Play() {   
-        String j1 = Pseudo.newPseudo();
-        String j2 = Pseudo.newPseudo();
-
-        System.out.println("\n\nGrille de " + j1 + " :\n");
-        Grille.afficherGrille(j1Grille);
-        System.out.println("\n\nGrille de " + j2 + " :\n");
-        Grille.afficherGrille(j2Grille);
-
-        while (fini_jeu == false) {
-            while (naviretouche == true) {
-                if (fini_jeu == true) {
-                    System.out.println("\n" + j1 + " a gagné la partie");
-                    naviretouche = false;
-                } else {
-                    System.out.println("\n" + j1 + " à vous de tirer !");
-                    Tirer(j2Grille, j1, j2);
-                }
+        System.out.println(message());
+        messageMenu();
+        int choix = Integer.parseInt(sc.nextLine());
+        while (choix != 2) {
+            switch (choix){
+                case 1:
+                    String gagnant = Index.Play();
+                    System.out.println("\n" + gagnant + " a gagné la partie !!!!");
+                    break;
+                default:
+                    System.out.println("Erreur!! Veuillez entrer un choisir une des deux options: ");
+                    break;
             }
-
-            naviretouche = true;
-            
-            while (naviretouche == true) {
-                if (fini_jeu == true) {
-                    System.out.println("\n" + j2 + " a gagné la partie");
-                    naviretouche = false;
-                } else {
-                    System.out.println("\n" + j2 + " à vous de tirer !");
-                    Tirer(j1Grille, j1, j2);
-                }
-            }
-
-            naviretouche = true;
-
+            System.out.println();
+            messageMenu();
+            choix = Integer.parseInt(sc.nextLine());
         }
-
-    }
-
-    public static void main(String[] args) {
-        Menu.launch();
+        if (choix == 2) {
+            clearConsole();
+        }
+        sc.close();
     }
 }
