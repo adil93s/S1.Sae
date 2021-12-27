7b18f00426d3bd89fdf9e34d06376190fb093f30
onizuka93 <42942677+onizuka93@users.noreply.github.com>
2021-12-28 00:13:44 +0100

    Update Grille.java

--- Jeu/Grille.java
+++ Jeu/Grille.java
@@ -11,24 +11,86 @@ public class Grille {
     public static final String ANSI_WHITE = "\u001B[37m";
 
     public static String[][] creerGrille() {
-        String[][] grilleJoueur = new String[11][11];
+        String[][] grilleJoueur = new String[10][10];
         int ligne;
         int colonne;
-        for (ligne = 0; ligne < grilleJoueur.length - 1; ligne++) {
-            for (colonne = 0; colonne < (grilleJoueur[ligne].length - 1); colonne++) {
+        for (ligne = 0; ligne < grilleJoueur.length; ligne++) {
+            for (colonne = 0; colonne < grilleJoueur[ligne].length; colonne++) {
                 grilleJoueur[ligne][colonne] = "~";
             }
         }
         int x = 0; 
         int y = 0; 
+        for (int compteur = 0; compteur < 4; compteur++) {
+            x = (int) (Math.random() * 10);
+            y = (int) (Math.random() * 10);
+            if (y != 0  && x != 0 && x != 9 && y != 9) {
+                if (grilleJoueur[x][y] == "□" || grilleJoueur[x][y-1] == "□" || grilleJoueur[x-1][y] == "□" || grilleJoueur[x][y+1] == "□" || grilleJoueur[x+1][y] == "□" || grilleJoueur[x+1][y+1] == "□" || grilleJoueur[x-1][y-1] == "□" || grilleJoueur[x-1][y+1] == "□" || grilleJoueur[x+1][y-1] == "□") {
+                    grilleJoueur[x][y] = "□";
+                } else {
+                    while (grilleJoueur[x][y] == "□" || grilleJoueur[x][y-1] == "□" || grilleJoueur[x-1][y] == "□" || grilleJoueur[x][y+1] == "□" || grilleJoueur[x+1][y] == "□" || grilleJoueur[x+1][y+1] == "□" || grilleJoueur[x-1][y-1] == "□" || grilleJoueur[x-1][y+1] == "□" || grilleJoueur[x+1][y-1] == "□") {
+                        x = (int) (Math.random() * 10);
+                        y = (int) (Math.random() * 10);
+                    }
+                    grilleJoueur[x][y] = "□";
+                }
+            }
+        }
+
         for (int compteur = 0; compteur < 3; compteur++) {
             x = (int) (Math.random() * 10);
             y = (int) (Math.random() * 10);
-            if(y != 0) {
-                grilleJoueur[x][y] = "□";
-                grilleJoueur[x][y - 1] = "□";
+            double z = Math.random();
+            if (y != 0  && x != 0 && x != 9 && y != 9) {
+                if (grilleJoueur[x][y] == "□" || grilleJoueur[x][y-1] == "□" || grilleJoueur[x-1][y] == "□" || grilleJoueur[x][y+1] == "□" || grilleJoueur[x+1][y] == "□" || grilleJoueur[x+1][y+1] == "□" || grilleJoueur[x-1][y-1] == "□" || grilleJoueur[x-1][y+1] == "□" || grilleJoueur[x+1][y-1] == "□") {
+                    grilleJoueur[x][y] = "□";
+                    if (z < 0.5) {
+                        grilleJoueur[x-1][y] = "□";
+                    } else {
+                        grilleJoueur[x][y-1] = "□";
+                    }
+                } else {
+                    while (grilleJoueur[x][y] == "□" || grilleJoueur[x][y-1] == "□" || grilleJoueur[x-1][y] == "□" || grilleJoueur[x][y+1] == "□" || grilleJoueur[x+1][y] == "□" || grilleJoueur[x+1][y+1] == "□" || grilleJoueur[x-1][y-1] == "□" || grilleJoueur[x-1][y+1] == "□" || grilleJoueur[x+1][y-1] == "□") {
+                        x = (int) (Math.random() * 10);
+                        y = (int) (Math.random() * 10);
+                    }
+                    grilleJoueur[x][y] = "□";
+                    if (z < 0.5) {
+                        grilleJoueur[x-1][y] = "□";
+                    } else {
+                        grilleJoueur[x][y-1] = "□";
+                    }                }
             }
         }
+        x = (int) (Math.random() * 10);
+        y = (int) (Math.random() * 10);
+        double z = Math.random();
+        if (y != 0  && x != 0 && x != 9 && y != 9) {
+            if (grilleJoueur[x][y] == "□" || grilleJoueur[x][y-1] == "□" || grilleJoueur[x-1][y] == "□" || grilleJoueur[x][y+1] == "□" || grilleJoueur[x+1][y] == "□" || grilleJoueur[x+1][y+1] == "□" || grilleJoueur[x-1][y-1] == "□" || grilleJoueur[x-1][y+1] == "□" || grilleJoueur[x+1][y-1] == "□") {
+                grilleJoueur[x][y] = "□";
+                if (z < 0.5) {
+                    grilleJoueur[x-1][y] = "□";
+                    grilleJoueur[x+1][y] = "□";
+                } else {
+                    grilleJoueur[x][y-1] = "□";
+                    grilleJoueur[x][y+1] = "□";
+                }
+            } else {
+                while (grilleJoueur[x][y] == "□" || grilleJoueur[x][y-1] == "□" || grilleJoueur[x-1][y] == "□" || grilleJoueur[x][y+1] == "□" || grilleJoueur[x+1][y] == "□" || grilleJoueur[x+1][y+1] == "□" || grilleJoueur[x-1][y-1] == "□" || grilleJoueur[x-1][y+1] == "□" || grilleJoueur[x+1][y-1] == "□") {
+                    x = (int) (Math.random() * 10);
+                    y = (int) (Math.random() * 10);
+                }
+                grilleJoueur[x][y] = "□";
+                if (z < 0.5) {
+                    grilleJoueur[x-1][y] = "□";
+                    grilleJoueur[x+1][y] = "□";
+                } else {
+                    grilleJoueur[x][y-1] = "□";
+                    grilleJoueur[x][y+1] = "□";
+                }
+            }
+        }          
+
         return grilleJoueur;
     }
 
@@ -36,7 +98,7 @@ public class Grille {
         int ligne;
         int colonne;
         System.out.println("     1 2 3 4 5 6 7 8 9 10");
-        for (ligne = 0; ligne < (grilleJoueur.length - 1); ligne++) {
+        for (ligne = 0; ligne < grilleJoueur.length; ligne++) {
             if (ligne == 0) {
                 for (int repeat = 0; repeat < 26; repeat++) {
                     if (repeat < 3)
@@ -51,7 +113,7 @@ public class Grille {
             } else {
                 System.out.print((ligne+1) + " ");
             }
-            for (colonne = 0; colonne < (grilleJoueur[ligne].length - 1); colonne++) {
+            for (colonne = 0; colonne < grilleJoueur[ligne].length; colonne++) {
                 if (colonne == 0) {
                     System.out.print("■ ");
                     System.out.print(ANSI_CYAN + grilleJoueur[ligne][colonne] + " " + ANSI_RESET);
