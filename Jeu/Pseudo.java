0be3b4c5bbca767d527dd42dccae859787eb836e
onizuka93 <42942677+onizuka93@users.noreply.github.com>
2021-12-29 19:30:19 +0100

    Update Pseudo.java

--- Jeu/Pseudo.java
+++ Jeu/Pseudo.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Pseudo {
     
     public static String newPseudo (){
+        
         Scanner saisie = new Scanner (System.in);
         System.out.println("Veuillez entrer votre pseudo : ");
         String name = saisie.nextLine();
         while(name.matches("^-?\\d+$")){
-            System.out.println("Erreur ! Veuillez entrer un pseudo contenant au moin un caractère : ");
+            System.out.println("Votre pseudo doit contenir au moin un caractère : ");
             name = saisie.nextLine();
         }
-        System.out.println("\nBienvenue Amiral " + name + "\n");
+        System.out.println("\nBienvenue Capitaine " + name + " ! Prêt pour la bataille ⚔️ ?\n");
         return name;
     }
 }
