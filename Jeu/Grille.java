b647570ac677de4bd8a741fef71f68ab7e09e071
onizuka93 <42942677+onizuka93@users.noreply.github.com>
2022-01-12 21:51:14 +0100

    Update Grille.java

--- Jeu/Grille.java
+++ Jeu/Grille.java
@@ -10,7 +10,7 @@ public class Grille {
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
 
-    public static int coordonneNavire() {
+    public static int genereCoordonne() {
         //Génération coordonné aleatoirement
         int min = 1;
         int max = 8;
@@ -24,62 +24,28 @@ public class Grille {
         if (tailleNavire == 1) {
             for (int compteur = 0; compteur < nombreNavire; compteur++) {
 
-                int x = coordonneNavire();
-                int y = coordonneNavire();
+                int x = genereCoordonne();
+                int y = genereCoordonne();
 
                 //Condition si presénce d'un navire ou d'un navire proche
                 boolean conditionPasDeNavire = grilleJoueur[x][y] != "□";
-                boolean conditionPasDeNavireProche = grilleJoueur[x][y-1] != "□" && grilleJoueur[x-1][y] != "□" && grilleJoueur[x][y+1] != "□" && grilleJoueur[x+1][y] != "□" && grilleJoueur[x+1][y+1] != "□" && grilleJoueur[x-1][y-1] != "□" && grilleJoueur[x-1][y+1] != "□" && grilleJoueur[x+1][y-1] != "□";
+                boolean conditionPasDeNavireProche = grilleJoueur[x][y-1] != "□" &&  grilleJoueur[x-1][y] != "□" && grilleJoueur[x][y+1] != "□" && grilleJoueur[x+1][y] != "□" && grilleJoueur[x+1][y+1] != "□" && grilleJoueur[x-1][y-1] != "□" && grilleJoueur[x-1][y+1] != "□" && grilleJoueur[x+1][y-1] != "□";
 
                 if (conditionPasDeNavire && conditionPasDeNavireProche) {
                     grilleJoueur[x][y] = "□";
                 } else {
                     while (grilleJoueur[x][y] == "□" || grilleJoueur[x][y-1] == "□" || grilleJoueur[x-1][y] == "□" || grilleJoueur[x][y+1] == "□" || grilleJoueur[x+1][y] == "□" || grilleJoueur[x+1][y+1] == "□" || grilleJoueur[x-1][y-1] == "□" || grilleJoueur[x-1][y+1] == "□" || grilleJoueur[x+1][y-1] == "□") {
-                        x = coordonneNavire();
-                        y = coordonneNavire();
+                        x = genereCoordonne();
+                        y = genereCoordonne();
                     }
                     grilleJoueur[x][y] = "□";
-                }
-            }
-        } else if (tailleNavire == 2) {
-            for (int compteur = 0; compteur < nombreNavire; compteur++) {
-
-                int x = coordonneNavire();
-                int y = coordonneNavire();
-
-
-                //Condition si presénce d'un navire ou d'un navire proche
-                boolean conditionPasDeNavire = grilleJoueur[x][y] != "□";
-                boolean conditionPasDeNavireProche = grilleJoueur[x][y-1] != "□" && grilleJoueur[x-1][y] != "□" && grilleJoueur[x][y+1] != "□" && grilleJoueur[x+1][y] != "□" && grilleJoueur[x+1][y+1] != "□" && grilleJoueur[x-1][y-1] != "□" && grilleJoueur[x-1][y+1] != "□" && grilleJoueur[x+1][y-1] != "□";
-
-                //Génération d'un double compris entre 0 et 1
-                double alea = Math.random();
-
-                if (conditionPasDeNavire && conditionPasDeNavireProche) {
-                    grilleJoueur[x][y] = "□";
-                    if (alea < 0.5) {
-                        grilleJoueur[x-1][y] = "□";
-                    } else {
-                        grilleJoueur[x][y-1] = "□";
-                    }                
-                } else {
-                    while (grilleJoueur[x][y] == "□" || grilleJoueur[x][y-1] == "□" || grilleJoueur[x-1][y] == "□" || grilleJoueur[x][y+1] == "□" || grilleJoueur[x+1][y] == "□" || grilleJoueur[x+1][y+1] == "□" || grilleJoueur[x-1][y-1] == "□" || grilleJoueur[x-1][y+1] == "□" || grilleJoueur[x+1][y-1] == "□") {
-                        x = coordonneNavire();
-                        y = coordonneNavire();
-                    }
-                    grilleJoueur[x][y] = "□";
-                    if (alea < 0.5) {
-                        grilleJoueur[x-1][y] = "□";
-                    } else {
-                        grilleJoueur[x][y-1] = "□";
-                    }                
                 }
             }
         } else {
             for (int compteur = 0; compteur < nombreNavire; compteur++) {
-                
-                int x = coordonneNavire();
-                int y = coordonneNavire();
+
+                int x = genereCoordonne();
+                int y = genereCoordonne();
 
 
                 //Condition si presénce d'un navire ou d'un navire proche
@@ -93,27 +59,23 @@ public class Grille {
                     grilleJoueur[x][y] = "□";
                     if (alea < 0.5) {
                         grilleJoueur[x-1][y] = "□";
-                        grilleJoueur[x+1][y] = "□";
                     } else {
                         grilleJoueur[x][y-1] = "□";
-                        grilleJoueur[x][y+1] = "□";                    
-                    }                 
+                    }                
                 } else {
                     while (grilleJoueur[x][y] == "□" || grilleJoueur[x][y-1] == "□" || grilleJoueur[x-1][y] == "□" || grilleJoueur[x][y+1] == "□" || grilleJoueur[x+1][y] == "□" || grilleJoueur[x+1][y+1] == "□" || grilleJoueur[x-1][y-1] == "□" || grilleJoueur[x-1][y+1] == "□" || grilleJoueur[x+1][y-1] == "□") {
-                        x = coordonneNavire();
-                        y = coordonneNavire();
+                        x = genereCoordonne();
+                        y = genereCoordonne();
                     }
                     grilleJoueur[x][y] = "□";
                     if (alea < 0.5) {
                         grilleJoueur[x-1][y] = "□";
-                        grilleJoueur[x+1][y] = "□";
                     } else {
                         grilleJoueur[x][y-1] = "□";
-                        grilleJoueur[x][y+1] = "□";                    
                     }                
                 }
-            }        
-        }
+            }
+        } 
     }
 
     public static String[][] creerGrille() {
@@ -125,11 +87,20 @@ public class Grille {
                 grilleJoueur[ligne][colonne] = "~";
             }
         }
-        placementNavire(grilleJoueur, 3, 1);
         placementNavire(grilleJoueur, 2, 3);
         placementNavire(grilleJoueur, 1, 4);
 
         return grilleJoueur;
+    }
+
+    public static void afficherEnLigneLn(int longueur) {
+        for (int repeat = 0; repeat < longueur; repeat++) {
+            if (repeat < 3)
+                System.out.print(" ");
+            else
+                System.out.print("■");
+        }
+        System.out.println();  
     }
 
     public static void afficherGrille(String[][] grilleJoueur) {
@@ -139,13 +110,7 @@ public class Grille {
         for (ligne = 0; ligne < grilleJoueur.length; ligne++) {
             if (ligne == 0) {
                 System.out.print("\t\t\t");
-                for (int repeat = 0; repeat < 26; repeat++) {
-                    if (repeat < 3)
-                        System.out.print(" ");
-                    else
-                        System.out.print("■");
-                }
-                System.out.println();
+                afficherEnLigneLn(26);
             }  
             if (ligne != 9) {
                 System.out.print("\t\t\t" + (ligne+1) + "  ");
@@ -163,13 +128,7 @@ public class Grille {
             System.out.print("■\n"); 
         }
         System.out.print("\t\t\t");
-        for (int repeat = 0; repeat < 26; repeat++) {
-            if (repeat < 3)
-                System.out.print(" ");
-            else
-                System.out.print("■");
-        }
-        System.out.println();      
+        afficherEnLigneLn(26); 
     }
 
 }
