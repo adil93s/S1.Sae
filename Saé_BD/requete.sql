9cf079dfad1f47c217af2ae0a235cfb9e9fbe982
IsmailSKR <95474812+IsmailSKR@users.noreply.github.com>
2022-01-18 20:10:14 +0100

    Update requete.sql

--- Saé_BD/requete.sql
+++ Saé_BD/requete.sql
@@ -6,3 +6,9 @@ select sum(prix) from reservation;
 
 select nom, ville_reservation from reservation natural join client where id_statut = 02;
 /*Afficher les noms des clients et la ville où la reservation a été annulé*/
+select nom from class natural join client where classe='Economique';
+/* Affiche le nom des clients qui sont en classe économique*/
+select avg(prix) from reservation;
+/*Affiche la moyenne des prix de réservation*/
+select count(nom) from client where nationalite='francaise';
+/* Affiche le nombre de client de nationalité française*/
