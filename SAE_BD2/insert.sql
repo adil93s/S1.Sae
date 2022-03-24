cb1666ab0e9fd73f470d536da63a74a5e6360461
So937 <101273593+So937@users.noreply.github.com>
2022-03-24 13:33:32 +0100

    Update insert.sql

--- SAE_BD2/insert.sql
+++ SAE_BD2/insert.sql
@@ -8,13 +8,13 @@ INSERT into pays(idpays ,nom_pays ,nom_continent)
 
 INSERT INTO cas(idcas, newcase_million, newcase, test_per_case, dates, idpays)
  values
- (1,,,,1),
- (2,,,,2),
- (3,,,,3),
- (4,,,,4),
- (5,,,,5);
+ (1,,,,,1),
+ (2,,,,,2),
+ (3,,,,,3),
+ (4,,,,,4),
+ (5,,,,,5);
  
- INSERT INTO mort(idmort, new_mort_million, new_mort, dates, idpays)
+ INSERT INTO positive(idpositive, rate, dates, idpays)
   values
   (1,,,1),
   (2,,,2),
