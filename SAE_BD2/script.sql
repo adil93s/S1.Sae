a98b5d513db80b2125bd6c85c40932d09b7ebd91
Adil <42942677+adil93s@users.noreply.github.com>
2022-03-24 13:40:30 +0100

    Update script.sql

--- SAE_BD2/script.sql
+++ SAE_BD2/script.sql
@@ -1,3 +1,6 @@
+drop schema if exist covidata;
+create schema covidata;
+
 drop table if existes vaccination cascade;
 drop table if existes pays cascade;
 drop table if existes cas cascade;
@@ -5,12 +8,12 @@ drop table if existes mort cascade;
 drop table if existes positive cascade;
 drop table if existes reproduction cascade;
 drop table if existes restriction cascade;
-CREATE TABLE pays(
+CREATE TABLE covidata.pays(
     idpays INT NOT NULL,
     nom_pays VARCHAR(100),
     nom_continent VARCHAR(100),
 );
-CREATE TABLE cas(
+CREATE TABLE covidata.cas(
     idcas INT NOT NULL,
     newcase_million INT NOT NULL,
     newcase INT NOT NULL,
@@ -20,7 +23,7 @@ CREATE TABLE cas(
     CONSTRAINT pk_cas PRIMARY KEY (idcas),
     CONSTRAINT fk_pays FOREIGN KEY (idpays) REFERENCES pays (idpays) 
 );
-CREATE TABLE mort(
+CREATE TABLE covidata.mort(
     idmort INT NOT NULL,
     new_mort_million INT NOT NULL, 
     new_mort INT NOT NULL,
@@ -29,7 +32,7 @@ CREATE TABLE mort(
     CONSTRAINT pk_mort PRIMARY KEY (idmort),
     CONSTRAINT fk_pays FOREIGN KEY (idpays) REFERENCES pays (idpays)
 );
-CREATE TABLE positive(
+CREATE TABLE covidata.positive(
     idpositive INT NOT NULL,
     rate INT NOT NULL,
     dates date,
@@ -37,7 +40,7 @@ CREATE TABLE positive(
     CONSTRAINT pk_positive PRIMARY KEY (idpositive),
     CONSTRAINT fk_pays FOREIGN KEY (idpays) REFERENCES pays (idpays)
 );
-CREATE TABLE reproduction(
+CREATE TABLE covidata.reproduction(
     idreproduction INT NOT NULL,
     rate INT NOT NULL,
     dates date,
@@ -45,7 +48,7 @@ CREATE TABLE reproduction(
     CONSTRAINT pk_reproduction PRIMARY KEY (idreproduction),
     CONSTRAINT fk_pays FOREIGN KEY (idpays) REFERENCES pays (idpays)
 );
-CREATE TABLE vaccination(
+CREATE TABLE covidata.vaccination(
     idvaccination INT NOT NULL,
     new_vaccination INT NOT NULL,
     newvaccination_smoothed INT NOT NULL,
@@ -54,10 +57,10 @@ CREATE TABLE vaccination(
     CONSTRAINT pk_vaccination PRIMARY KEY (idvaccination),
     CONSTRAINT fk_pays FOREIGN KEY (idpays) REFERENCES pays (idpays)
 );
-CREATE TABLE restriction(
+CREATE TABLE covidata.restriction(
     idrestriction INT NOT NULL,
     dates date,
     idpays INT NOT NULL,
     CONSTRAINT pk_restriction PRIMARY KEY (idrestriction),
     CONSTRAINT fk_pays FOREIGN KEY (idpays) REFERENCES pays (idpays)
-    );
+);
