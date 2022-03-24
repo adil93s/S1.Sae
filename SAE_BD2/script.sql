95fd5677d41ce685afdb058b59e91546b719247c
IsmailSKR <95474812+IsmailSKR@users.noreply.github.com>
2022-03-24 13:03:57 +0100

    Update script.sql

--- SAE_BD2/script.sql
+++ SAE_BD2/script.sql
@@ -4,11 +4,11 @@ drop table if existes cas cascade;
 drop table if existes mort cascade;
 drop table if existes positive cascade;
 drop table if existes reproduction cascade;
+drop table if existes restriction cascade;
 CREATE TABLE pays(
     idpays INT NOT NULL,
     nom_pays VARCHAR(100),
     nom_continent VARCHAR(100),
-    stringecy_index INT NOT NULL,
 );
 CREATE TABLE cas(
     idcas INT NOT NULL,
@@ -54,3 +54,10 @@ CREATE TABLE vaccination(
     CONSTRAINT pk_vaccination PRIMARY KEY (idvaccination),
     CONSTRAINT fk_pays FOREIGN KEY (idpays) REFERENCES pays (idpays)
 );
+CREATE TABLE restriction(
+    idrestriction INT NOT NULL,
+    dates date,
+    idpays INT NOT NULL,
+    CONSTRAINT pk_restriction PRIMARY KEY (idrestriction),
+    CONSTRAINT fk_pays FOREIGN KEY (idpays) REFERENCES pays (idpays)
+    );
