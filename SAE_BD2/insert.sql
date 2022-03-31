d1213d940cdf309ab7c260fec8f614f6de1f28e6
So937 <101273593+So937@users.noreply.github.com>
2022-03-31 14:43:55 +0200

    Update insert.sql

--- SAE_BD2/insert.sql
+++ SAE_BD2/insert.sql
@@ -24,11 +24,11 @@ INSERT INTO covidata.cas(idcas, newcase_million, newcase, test_per_case, dates, 
 
 INSERT INTO covidata.mort(idmort, new_mort_million, new_mort, dates, idpays)
     Values
-  (1,,12/04/2021,1),
-  (2,0.397,08/02/2022,2),
-  (3,0.3031,09/02/2022,3),
-  (4,0.1076,12/02/2022,4),
-  (5,0.328,06/02/2022,5);
+  (1,,,12/04/2021,1),
+  (2,,0.397,08/02/2022,2),
+  (3,,0.3031,09/02/2022,3),
+  (4,,0.1076,12/02/2022,4),
+  (5,,0.328,06/02/2022,5);
 
 INSERT INTO covidata.reproduction(idreproduction, rate, dates, idpays)
     Values
