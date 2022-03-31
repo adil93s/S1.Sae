5c6cad01076b73f54c4a4ee2c1aef266adb677bc
So937 <101273593+So937@users.noreply.github.com>
2022-03-31 12:42:30 +0200

    Update insert.sql

--- SAE_BD2/insert.sql
+++ SAE_BD2/insert.sql
@@ -8,7 +8,7 @@ INSERT into covidata.pays(idpays ,nom_pays ,nom_continent)
 
 INSERT INTO covidata.cas(idcas, newcase_million, newcase, test_per_case, dates, idpays)
  values
- (1,,,,12/02/2022,1),
+ (1,465,36,7.8,12/02/2022,1),
  (2,3072,26771,2,08/02/2022,2),
  (3,1139,5677,3,09/02/2022,3),
  (4,1030,62221,9,12/02/2022,4),
@@ -16,7 +16,7 @@ INSERT INTO covidata.cas(idcas, newcase_million, newcase, test_per_case, dates, 
  
  INSERT INTO covidata.positive(idpositive, rate, dates, idpays)
   values
-  (1,,,1),
+  (1,0.1281,12/04/2021,1),
   (2,0.397,08/02/2022,2),
   (3,0.3031,09/02/2022,3),
   (4,0.1076,12/02/2022,4),
@@ -24,7 +24,7 @@ INSERT INTO covidata.cas(idcas, newcase_million, newcase, test_per_case, dates, 
 
 INSERT INTO covidata.mort(idmort, rate, dates, idpays)
     Values
-  (1,,,1),
+  (1,,12/04/2021,1),
   (2,0.397,08/02/2022,2),
   (3,0.3031,09/02/2022,3),
   (4,0.1076,12/02/2022,4),
@@ -32,7 +32,7 @@ INSERT INTO covidata.mort(idmort, rate, dates, idpays)
 
 INSERT INTO covidata.reproduction(idreproduction, rate, dates, idpays)
     Values
-  (1,,,1),
+  (1,,12/04/2021,1),
   (2,0.397,08/02/2022,2),
   (3,0.3031,09/02/2022,3),
   (4,0.1076,12/02/2022,4),
@@ -40,7 +40,7 @@ INSERT INTO covidata.reproduction(idreproduction, rate, dates, idpays)
 
 INSERT INTO covidata.vaccination(idvaccination, new_vaccination, newvaccination_smoothed, dates, idpays) 
    Values
-  (1,,,,1),
+  (1,,,12/04/2021,1),
   (2,,,08/02/2022,2),
   (3,,,09/02/2022,3),
   (4,,,12/02/2022,4),
@@ -48,7 +48,7 @@ INSERT INTO covidata.vaccination(idvaccination, new_vaccination, newvaccination_
   
  INSERT INTO covidata.restriction(idvaccination, stringency_index, dates, idpays) 
    Values
-  (1,35.19,12/02/2022,1),
+  (1,35.19,12/04/2021,1),
   (2, ,08/02/2022,2),
   (3,,09/02/2022,3),
   (4,,12/02/2022,4),
