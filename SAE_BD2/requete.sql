717f1b2d0ffceb38847838566fe7a54de53880bd
Adil <42942677+adil93s@users.noreply.github.com>
2022-04-03 14:13:29 +0200

    Update requete.sql

--- SAE_BD2/requete.sql
+++ SAE_BD2/requete.sql
@@ -1,8 +1,8 @@
-/*Moyenne des nouveaux cas des 5 pays d'Europe*/
-SELECT avg(newcase) FROM covidata.cas;
+/*Moyenne des nouveaux cas en France*/
+SELECT avg(newcase) FROM covidata.cas NATURAL JOIN covidata.pays WHERE nom_pays = 'France';
 
-/*Variance du taux positive des 5 pays d'Europe */
-SELECT variance(rate) FROM covidata.positive;
+/*Variance du taux positive des pays d'Europe en 2022*/
+SELECT variance(rate) FROM covidata.positive WHERE dates > '2022-01-01';
 
 /*Ecart type des nouvellles vaccinations des 5 pays d'Europe*/
 SELECT stddev(new_vaccination) FROM covidata.vaccination;
